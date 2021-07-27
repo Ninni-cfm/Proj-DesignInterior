3c0480cbad223650078504ee153b023cebdc56ab
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-27 16:40:44 +0200

    Desktop version complete, mobile started

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,21 @@
 // set Copyright
 document.getElementById("copyright").innerHTML = "COPYRIGHT &copy; " + new Date().getFullYear();
+
+let user = document.getElementById("name");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+
+// submit button was clicked
+function submitData() {
+
+    let msg = user.value + " <" + email.value + ">, you have sucessfully signed up to H & H!\n";
+    msg += "Your message:\n";
+    msg += message.value;
+
+    console.log(msg);
+
+    user.value = "";
+    email.value = "";
+    message.value = "";
+}
+
